4d7b21e9c44151839165a2aefa0ab0e59427a850
Rana123Mandeep <writetomandeeprana@gmail.com>
2025-10-06 08:57:43 +1300

    index .html replace by login .html and modified app.py

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
-
+import os
 
 
 app = Flask(__name__)  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mandeepsingh@localhost:5432/thrift store _db'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.urandom(24)
 db = SQLAlchemy()
 db.init_app(app)
 migrate=Migrate(app, db)
@@ -17,8 +18,8 @@ migrate=Migrate(app, db)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "your_email@gmail.com"   
-app.config['MAIL_PASSWORD'] = "your_app_password"      
+app.config['MAIL_USERNAME'] = "customercarethriftstore@gmail.com"   
+app.config['MAIL_PASSWORD'] =  "your_16_char_app_password"     
 mail = Mail(app)
 
 
@@ -40,21 +41,22 @@ def add_user():
     temp_email = "sunnygandhi548@gmail.com"
     temp_password = "mash123"  
 
-    new_user = User(
-        first_name=temp_first_name,
-        last_name=temp_last_name,
-        email=temp_email,
-        password=temp_password,
-    )
+    # new_user = User(
+    #     first_name=temp_first_name,
+    #     last_name=temp_last_name,
+    #     email=temp_email,
+    #     password=temp_password,
+    # )
 
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-        return "Added New user"
-    except Exception as e:
-        db.session.rollback()
-        return f"Error: {e}"
+    # try:
+    #     db.session.add(new_user)
+    #     db.session.commit()
+    #     return "Added New user"
+    # except Exception as e:
+    #     db.session.rollback()
+    #     return f"Error: {e}"
     
+        
 @app.route("/Signup", methods=["GET", "POST"])
 def Signup():
     if request.method == "POST":
@@ -64,16 +66,57 @@ def Signup():
         password = request.form["password"]
         confirm_password = request.form["confirm_password"]
 
-        if password!= confirm_password:
-             flash("Passwords do not match!", "Retry")
-             return redirect(url_for("signup"))
+        if password != confirm_password:
+            flash("Passwords do not match!", "Retry")
+            return redirect(url_for("Signup"))
         
+        if User.query.filter_by(email=email).first():
+            flash("Email already registered. Please login.", "danger")
+            return redirect(url_for("Signup"))
+
+
         hashed_password = generate_password_hash(password)
 
-        #send mail 
-        
-        
+        new_user = User(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=hashed_password,
+        )
 
+        try:
+            # Save to DB
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Send email
+            msg = Message(
+                subject="Account verification 🎉",
+                sender=app.config['MAIL_USERNAME'],
+                recipients=[email]
+            )
+            msg.body = f"""
+            Hi {first_name},
+
+            Congratulations! Your account has been verified and Registered  successfully.
+            You can now log in.
+
+            Best regards,  
+            Thrift Store App Team
+            """
+            mail.send(msg)
+
+            flash("User created successfully! Check your email.", "success")
+            return redirect(url_for("login")) 
+
+        except Exception as e:
+            db.session.rollback()
+            return f"Error: {e}"
+
+    return render_template("Signup.html")
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 
